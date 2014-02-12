f05c4d1620cb27e9c7ceb8f905aae365a2406016
ArturAmbartsumov <Artur_pskov@mail.ru>
2014-02-12 21:37:50 +0400

    Commit

---
+++ JavaProject/src/main/java/HelloServer.java
@@ -0,0 +1,10 @@
+/**
+ * Created by artur on 12.02.14.
+ */
+import java.lang.System;
+
+public class HelloServer {
+    public static void main(String[] atga) {
+        System.out.println("Hello Server");
+    }
+}
