6b687b5063550d7c1ce95cfe879d06c3d4e888b0
ArturAmbartsumov <Artur_pskov@mail.ru>
2014-02-13 21:58:05 +0400

    Добавлен простой сервер jetty

--- JavaProject/src/main/java/HelloServer.java
+++ JavaProject/src/main/java/HelloServer.java
@@ -1,10 +1,36 @@
 /**
  * Created by artur on 12.02.14.
  */
-import java.lang.System;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
 
-public class HelloServer {
-    public static void main(String[] atga) {
-        System.out.println("Hello Server");
+import java.io.IOException;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class HelloServer extends AbstractHandler
+{
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello Server</h1>");
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8080);
+        server.setHandler(new HelloServer());
+
+        server.start();
+        server.join();
     }
 }
