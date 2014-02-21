26e5b9ed5e1eef16bfb3cc99d73272675a5293a9
ArturAmbartsumov <Artur_pskov@mail.ru>
2014-02-22 03:27:08 +0400

    ДЗ 2 исправлены замечания

--- JavaProject/src/main/java/frontend/Frontend.java
+++ JavaProject/src/main/java/frontend/Frontend.java
@@ -2,6 +2,7 @@ package frontend;
 
 import templater.PageGenerator;
 
+import java.util.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +11,6 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -20,14 +18,51 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class Frontend extends HttpServlet {
 
-    private String login = "";
-    private String password = "";
-    private AtomicLong userIdGenerator = new AtomicLong();
+    public Frontend() {
+        User user1 = new User("Artur", "12345");
+        User user2 = new User("Ivan", "123");
+        userList.add(user1);
+        userList.add(user2);
+    }
 
-    public static String getTime() {
+    class User {
+        public String name;
+        public String password;
+        public User(String n, String p) {
+            name = n;
+            password = p;
+        }
+        public boolean equals(Object obj) {
+            if(obj == this)
+                return true;
+
+            if(obj == null)
+                return false;
+
+            if(!(getClass() == obj.getClass()))
+                return false;
+            else
+            {
+                User tmp = (User)obj;
+                if(tmp.name.equals(name) && tmp.password.equals(password))
+                    return true;
+                else
+                    return false;
+            }
+        }
+    }
+
+
+    private User currentUser = new User("", "");
+    private ArrayList<User> userList = new ArrayList<User>();
+    private AtomicLong userIdGenerator = new AtomicLong();
+    private boolean userChange = false;
+    DateFormat formatter = new SimpleDateFormat("HH.mm.ss");
+
+
+
+    public String getTime() {
         Date date = new Date();
-        date.getTime();
-        DateFormat formatter = new SimpleDateFormat("HH.mm.ss");
         return formatter.format(date);
     }
 
@@ -38,47 +73,61 @@ public class Frontend extends HttpServlet {
         Map<String, Object> pageVariables = new HashMap<>();
 
         if (request.getPathInfo().equals("/authform")) {
-            login = request.getParameter("login");
-            password = request.getParameter("password");
-            if (login != null && password != null) {
-                if (login.equals("Artur") && password.equals("12345")) {
-                    response.sendRedirect("/userid");
-                }
-                else {
-                    pageVariables.put("message", "Ошибка: не правильный логин или пароль!!");
-                    response.getWriter().println(PageGenerator.getPage("authform.tml", pageVariables));
-                }
-            }
-            else {
-                pageVariables.put("message", "Введите логин и пароль!");
-                response.getWriter().println(PageGenerator.getPage("authform.tml", pageVariables));
-            }
-            return;
+            authForm(request, response, pageVariables);
         }
 
         if (request.getPathInfo().equals("/timer")) {
-            pageVariables.put("refreshPeriod", "1000");
-            pageVariables.put("serverTime", getTime());
-            response.getWriter().println(PageGenerator.getPage("timer.tml", pageVariables));
-            return;
+            timer(request, response, pageVariables);
         }
 
         if (request.getPathInfo().equals("/userid")) {
-            HttpSession session = request.getSession();
-            Long userId = (Long) session.getAttribute("userId");
-            if (userId == null) {
-                userId = userIdGenerator.getAndIncrement();
-                session.setAttribute("userId", userId);
-            }
-            pageVariables.put("refreshPeriod", "1000");
-            pageVariables.put("serverTime", getTime());
-            pageVariables.put("userId", userId);
-            response.getWriter().println(PageGenerator.getPage("userid.tml", pageVariables));
-            return;
+            userId(request, response, pageVariables);
         }
     }
 
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    private void authForm(HttpServletRequest request,
+                          HttpServletResponse response,
+                          Map<String, Object> pageVariables) throws ServletException, IOException {
+        User logEndPassword = new User(request.getParameter("login"), request.getParameter("password"));
+        if (logEndPassword.name != null && logEndPassword.password != null) {
+            if (userList.contains(logEndPassword)) {
+                response.sendRedirect("/userid");
+            }
+            else {
+                pageVariables.put("message", "Ошибка: не правильный логин или пароль!!");
+                response.getWriter().println(PageGenerator.getPage("authform.tml", pageVariables));
+            }
+        }
+        else {
+            pageVariables.put("message", "Введите логин и пароль!");
+            response.getWriter().println(PageGenerator.getPage("authform.tml", pageVariables));
+        }
+    }
+
+    private void timer(HttpServletRequest request,
+                       HttpServletResponse response,
+                       Map<String, Object> pageVariables) throws ServletException, IOException {
+        pageVariables.put("refreshPeriod", "1000");
+        pageVariables.put("serverTime", getTime());
+        response.getWriter().println(PageGenerator.getPage("timer.tml", pageVariables));
+    }
+
+    private void userId(HttpServletRequest request,
+                        HttpServletResponse response,
+                        Map<String, Object> pageVariables) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Long userId = (Long) session.getAttribute("userId");
+        if (userId == null) {
+            userId = userIdGenerator.getAndIncrement();
+            session.setAttribute("userId", userId);
+        }
+        pageVariables.put("refreshPeriod", "1000");
+        pageVariables.put("serverTime", getTime());
+        pageVariables.put("userId", userId);
+        response.getWriter().println(PageGenerator.getPage("userid.tml", pageVariables));
     }
 }
